1254f97fb37c83af3b3b4c5920ec21d132c81838
santosh kumar thumu <sanjuthumu2004@gmail.com>
2026-02-26 12:10:11 +0530

    updated

--- sql/03_gold.sql
+++ sql/03_gold.sql
@@ -18,3 +18,26 @@ SELECT
 FROM
     SILVER.FACT_SALES 
 GROUP BY sales_year;
+--Sales by Store every month
+CREATE OR REPLACE TABLE GOLD.MONTHLY_SALES AS
+SELECT
+    DATE_TRUNC('month',transaction_date) as sales_month,
+    SUM(sales_price) AS total_sales
+FROM SILVER.FACT_SALES
+GROUP BY sales_month;
+
+--Top 10 stores by sales
+CREATE OR REPLACE TABLE GOLD.TOP_STORES AS
+SELECT
+     store_id,
+     SUM(sales_price) AS total_sales
+FROM SILVER.FACT_SALES
+GROUP BY store_id
+ORDER BY total_sales DESC
+LIMIT 10;
+SELECT COUNT(*) FROM GOLD.MONTHLY_SALES;
+SELECT COUNT(*) FROM GOLD.TOP_STORES;
+SELECT COUNT(*) FROM GOLD.SALES_BY_STATE;
+SELECT COUNT(*) FROM GOLD.SALES_BY_YAER;
+select sum(sales_price) from silver.fact_sales;
+select sum(total_sales) from gold.sales_by_state;
